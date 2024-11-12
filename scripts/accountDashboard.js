1e92c1861ddff73ed047a1e1b92fded46c0a32a5
David Dewanto <david.dewanto@gmail.com>
2024-11-12 18:56:45 +0700

    remove unecessary items

--- scripts/accountDashboard.js
+++ scripts/accountDashboard.js
@@ -1,42 +1,40 @@
 let activeModules = [1];
-let availableModules = [1,2,3,4];
+let availableModules = [1, 2, 3, 4];
 
-function getUserName(){
+function getUserName() {
   document.getElementById("user-id").innerHTML = localStorage.getItem("username");
 }
 
-function setActiveModules(){
-  for(const element of activeModules){
+function setActiveModules() {
+  for (const element of activeModules) {
     let temp = document.querySelector(`#module-${element} a`);
     temp.classList.add("active-module");
-    temp.setAttribute("href",`/account/exercise/module${element}.html`);
+    temp.setAttribute("href", `/account/exercise/module${element}.html`);
   }
 
   let nonActiveModules = availableModules.filter(i => !activeModules.includes(i));
   console.log(nonActiveModules);
 
-  for (const element of nonActiveModules){
+  for (const element of nonActiveModules) {
     let temp = document.querySelector(`#module-${element} a`);
     temp.classList.add("restricted-module");
     temp.addEventListener("click", showWarning);
   }
 }
 
-function showWarning(){
-  let temp = document.querySelector(".warning-message");
-  temp.classList.add("show");
+function showWarning() {
+  const warningMessage = document.querySelector(".warning-message");
+  warningMessage?.classList.add("show");
 }
 
-function closeWarning(){
-  let temp = document.querySelector(".warning-message");
-  temp.classList.remove("show");
+function closeWarning() {
+  const warningMessage = document.querySelector(".warning-message");
+  warningMessage?.classList.remove("show");
+  console.log("success");
 }
-
 document.addEventListener("DOMContentLoaded", () => {
   getUserName();
   setActiveModules();
-  const logoutBtn = document.getElementById("logout-btn");
-  logoutBtn.addEventListener("click", handleLogout);
-  const warningCloseBtn = document.getElementById("warning-close-btn");
-  warningCloseBtn.addEventListener("click", closeWarning);
+  
+  
 });
