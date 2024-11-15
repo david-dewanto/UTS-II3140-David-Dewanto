38e668e8da7a43e397b016238aaf94ab6a8adb6a
David Dewanto <david.dewanto@gmail.com>
2024-11-15 14:17:39 +0700

    change url

--- scripts/url.js
+++ scripts/url.js
@@ -1,2 +1,2 @@
-export const backendURL = "http://127.0.0.1:8000/";
-// export const backendURL = "https://uts-david-dewanto-back-end.vercel.app/";
+// export const backendURL = "http://127.0.0.1:8000/";
+export const backendURL = "https://uts-david-dewanto-back-end.vercel.app/";
